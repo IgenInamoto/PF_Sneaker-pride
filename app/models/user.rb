cbbce314771fd39e6896316802cf5cda0f0b2193
IgenInamoto <szancd45d@i.softbank.jp>
2023-06-15 11:29:26 +0000

    検索機能、コメント機能(非同期通信)完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,9 @@ class User < ApplicationRecord
   has_many :sneaker_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   
-  # 自分がフォローされる側の関係性
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  # 被フォロー関係を通じて参照→自分をフォローしている人
   has_many :followers, through: :reverse_of_relationships, source: :follower
-  
-  # 自分がフォローする側の関係性
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  # 与フォロー関係を通じて参照→自分がフォローしている人
   has_many :followings, through: :relationships, source: :followed
   
   has_one_attached :profile_image
@@ -28,16 +23,34 @@ class User < ApplicationRecord
     (profile_image.attached?) ? profile_image : 'default-image.jpg'
   end
   
+  # フォローした時の処理
   def follow(user)
     relationships.create(followed_id: user.id)
   end
-
+  
+  # フォローを外す時の処理
   def unfollow(user)
     relationships.find_by(followed_id: user.id).destroy
   end
-
+  
+  # フォローしているかの判定(フォローしていればtrueを返す)
   def following?(user)
     followings.include?(user)
+  end
+  
+   # 検索方法分岐
+  def self.looks(search, word)
+    if search == "perfect_match" #完全一致
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match" #前方一致
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match" #後方一致
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match" #部分一致
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
   end
          
 end
