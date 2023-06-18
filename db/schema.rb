2a7ef8ec96c7aa271190ff545dd146e770f1b4ad
IgenInamoto <szancd45d@i.softbank.jp>
2023-06-18 05:02:21 +0000

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2023_06_18_043546) do
-
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -94,8 +92,8 @@ ActiveRecord::Schema.define(version: 2023_06_18_043546) do
     t.text "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_valid", default: true, null: false
     t.boolean "is_deleted", default: false, null: false
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
