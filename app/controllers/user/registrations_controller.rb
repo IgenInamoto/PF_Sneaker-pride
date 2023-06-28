158ce573bf295dda53abb4261b771d43d1317f25
IgenInamoto <szancd45d@i.softbank.jp>
2023-06-28 07:34:27 +0000

    遷移先修正

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -4,6 +4,10 @@ class User::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
   before_action :configure_permitted_parameters
+  
+  def after_sign_in_path_for(resource)
+    user_path(current_user.id)
+  end
 
   # GET /resource/sign_up
   # def new
